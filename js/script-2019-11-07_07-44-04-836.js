23e8849c080b3c5f02392e0cb9078d38262ab024
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2019-11-07 08:16:01 +0100

    Remove class 'active' from article links and all articles

--- js/script-2019-11-07_07-44-04-836.js
+++ js/script-2019-11-07_07-44-04-836.js
@@ -12,24 +12,16 @@ const titleClickHandler = function (event) {
     console.log(event);
 
     /* remove class 'active' from all article links  */
+
     const activeLinks = document.querySelectorAll('.titles a.active');
-    console.log(activeLinks);
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
-        console.log(activeLink);
     }
-
+    
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.posts .active');
-    console.log(activeArticles);
-
-    for (let activeArticle of activeArticles) {
-        activeArticle.classList.remove('active');
-        console.log(activeArticle);
-    }
 
     /* get 'href' attribute from the clicked link */
 
